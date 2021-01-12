7d5fc30c6c468010c491c62413b040614edfe6ef
N1NJ4H <N1NJ4H@N1NJ4H.com>
2021-01-12 19:19:52 +0900

    delete debug message

--- cogs/worldtime.py
+++ cogs/worldtime.py
@@ -11,10 +11,8 @@ class WorldTime(commands.Cog):
 
     @commands.command(ignore_extra=False)
     async def time(self, ctx):
-        print("in time1")
         tokyo = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
         amsterdam = datetime.datetime.now(pytz.timezone('Europe/Amsterdam'))
-        print("in time2")
         await ctx.send('Tokyo: {}'.format(tokyo))
         await ctx.send('Amsterdam: {}'.format(amsterdam))
 
