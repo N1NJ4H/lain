87f655318d633b77cd5fe61da0af7a24b37bde75
N1NJ4H <N1NJ4H@N1NJ4H.com>
2021-01-12 20:13:56 +0900

    modified result message

--- cogs/currencyconverter.py
+++ cogs/currencyconverter.py
@@ -19,10 +19,10 @@ class CurrencyConverter(commands.Cog):
             # BTC 変換
             if c_from_upper == 'BTC':
                 cc = b.convert_btc_to_cur(float(value), c_to_upper)
-                await ctx.send('[{}] -> [{}] = {} -> {}'.format(c_from_upper, c_to_upper, float(value), cc))
+                await ctx.send('{}{} -> {}{}'.format(float(value), c_from_upper, cc, c_to_upper))
             if c_to_upper== 'BTC':
                 cc = b.convert_to_btc(float(value), c_from_upper)
-                await ctx.send('[{}] -> [{}] = {} -> {}'.format(c_from_upper, c_to_upper, float(value), cc))
+                await ctx.send('{}{} -> {}{}'.format(float(value), c_from_upper, cc, c_to_upper))
         else:
             cc = c.convert(c_from_upper, c_to_upper, float(value))
             await ctx.send('[{}] -> [{}] = {} -> {}'.format(c_from_upper, c_to_upper, float(value), cc))
